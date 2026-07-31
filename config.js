ce34757a4073d360d554307ffa435309a71f8366
wwwnicita1-cell <www.nicita.1@gmail.com>
2026-07-31 20:20:29 +0500

    Add files via upload

---
+++ config.js
@@ -0,0 +1,5 @@
+// Вставь токен бота между кавычками в строке ниже. Больше ничего не меняй.
+
+const TELEGRAM_BOT_TOKEN = "8351624267:AAGZmU1_0dBxzEuIID9vEd4KNYskqhpwFPw";
+
+const TELEGRAM_CHAT_ID = "560161007";
